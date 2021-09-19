06c7b019b9212ed65391a33b216cfbf3a8fb9016
italian6th <38963171+italian6th@users.noreply.github.com>
2021-09-18 21:19:53 -0700

    Create smocommand.js

---
+++ smocommand.js
@@ -0,0 +1,18 @@
+d=[ 
+'dream game SWOONER', 
+'it hurt my feelings to turn off the game kaestr1Cry', 
+'Cappy WHY ARE YOU SO POGGERS ?? pepeD', 
+'dodged bonk smolFlex', 
+'psychological THRILLER pcrowDab', 
+'psychological THRILLER pcrowDab', 
+'Y OOOO Luncheon Kingdom italia84Yay', 
+'precision input pepeD', 
+'heartwarming Kissahomie', 
+'get got, klepto! pcrowDab', 
+'smolBless', 
+"Kissahomie", 
+"that fokin' dragon SWOONER", 
+'Controller Connection: Stable italia84Yep', 
+"I'm LOVE kaestr1Hug", 
+'Wow! OBS just works! smolBrain' 
+]; d[Math.floor(Math.random()*d.length)]
